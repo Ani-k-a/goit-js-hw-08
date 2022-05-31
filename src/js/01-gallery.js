02d1cb1d2fbf7744c3392b200398aa80016d0406
Hanna Lytvynenko <ani-k-a@ukr.net>
2022-05-31 16:09:42 +0200

    Base ParcelOld + task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+function galleryItem() {
+    const markup = galleryItems
+    .map(
+        ({ original, preview, description }) => 
+         `<div> 
+            <a class="gallery__item" href="${original}">
+              <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+            </a>
+          </div>`)
+    .join('');
+    return markup;
+};
+
+gallery.insertAdjacentHTML("beforeend", galleryItem());
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
