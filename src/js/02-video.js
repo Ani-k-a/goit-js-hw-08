64bf5216ba85e6a5bd4a7f6dd7e65cd67f5f8be9
Hanna Lytvynenko <ani-k-a@ukr.net>
2022-06-05 08:33:05 +0200

    Task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import throttle from "lodash.throttle"
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+const onPlay = function(data) {
+    console.log(data.seconds);
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
 
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+
+
+form.addEventListener('input', throttle(onTextareaData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const formData = {};
+
+function onTextareaData(e) {
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state')
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')))
+}
+
+const textarea = document.querySelector('textarea');
+const email = document.querySelector('input');
+
+populateTextarea()
+
+function populateTextarea() {
+    const saveMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (saveMessage) {
+        textarea.value = saveMessage.message;
+        email.value = saveMessage.email;
+    }
+}
